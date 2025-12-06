2736d75bf43e70139cfe403ecaa90cc12b4dfb87
cypherpulse <johnbradill67@gmail.com>
2025-12-06 21:38:12 +0300

    feat: Added page component - Records - OnchainCare: A secure decentralised hospital management system built on Base, Ethereum L2 Blockchain Network

---
+++ src/pages/Records.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from '@/components/layout/DashboardLayout';
+import { PatientRecords } from '@/components/patient/PatientRecords';
+
+const Records = () => (
+  <DashboardLayout>
+    <PatientRecords />
+  </DashboardLayout>
+);
+
+export default Records;
