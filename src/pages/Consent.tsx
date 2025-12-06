d8179e1b2cfd9c2a90f378649d2b068ae7c3718f
cypherpulse <johnbradill67@gmail.com>
2025-12-06 21:38:10 +0300

    feat: Added page component - Consent - OnchainCare: A secure decentralised hospital management system built on Base, Ethereum L2 Blockchain Network

---
+++ src/pages/Consent.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from '@/components/layout/DashboardLayout';
+import { PatientConsent } from '@/components/patient/PatientConsent';
+
+const Consent = () => (
+  <DashboardLayout>
+    <PatientConsent />
+  </DashboardLayout>
+);
+
+export default Consent;
