3902d20f9d1e7b98796e5f350c03eb7b83ce8af4
cypherpulse <johnbradill67@gmail.com>
2025-12-06 21:38:08 +0300

    feat: Added page component - Billing - OnchainCare: A secure decentralised hospital management system built on Base, Ethereum L2 Blockchain Network

---
+++ src/pages/Billing.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from '@/components/layout/DashboardLayout';
+import { PatientBilling } from '@/components/patient/PatientBilling';
+
+const Billing = () => (
+  <DashboardLayout>
+    <PatientBilling />
+  </DashboardLayout>
+);
+
+export default Billing;
