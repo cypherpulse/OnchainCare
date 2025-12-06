60b8029a8314aed3a2cc273512e2d86b95988edf
cypherpulse <johnbradill67@gmail.com>
2025-12-06 21:38:08 +0300

    feat: Added page component - AccessLogs - OnchainCare: A secure decentralised hospital management system built on Base, Ethereum L2 Blockchain Network

---
+++ src/pages/AccessLogs.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from '@/components/layout/DashboardLayout';
+import { DoctorAccessLogs } from '@/components/doctor/DoctorAccessLogs';
+
+const AccessLogs = () => (
+  <DashboardLayout>
+    <DoctorAccessLogs />
+  </DashboardLayout>
+);
+
+export default AccessLogs;
